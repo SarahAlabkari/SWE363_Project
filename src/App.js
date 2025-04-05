506744dab0eeeb3768d0ddd502e86c89972f8f83
Norah <Norah.8782@hotmail.com>
2025-04-05 05:56:32 +0300

    Calendar, Cities DropDownMenu components, and Payment page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import DropdownMenu from './components/DropdownMenu';
+import CalendarComponent from './components/CalendarComponent';
+import PlanExperience from './pages/PlanExperience';
+import Payment from './pages/Payment';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Routes>
+    <Route path="/" element={<PlanExperience />} />
+    <Route path="/payment" element={<Payment />} />
+  </Routes>
   );
 }
 
