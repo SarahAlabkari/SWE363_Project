5db2994e77bae984a4c47f65309929ac6a54a2e8
Sarah Alabkari <Sa.alabkari@gmail.com>
2025-04-05 12:28:21 +0300

    feat: add an event component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import EventTryoutPage from './pages/EventTryoutPage';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/EventTryoutPage" element={<EventTryoutPage />}></Route>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
