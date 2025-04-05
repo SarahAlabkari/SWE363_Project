6ce1d1402c3fa46867927ef330e399f5eaded96e
Sarah Alabkari <Sa.alabkari@gmail.com>
2025-04-05 12:58:40 +0300

    fix: resolved merge issues

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EventTryoutPage from './pages/EventTryoutPage';
+import DropdownMenu from './components/DropdownMenu';
+import CalendarComponent from './components/CalendarComponent';
+import PlanExperience from './pages/PlanExperience';
+import Payment from './pages/Payment';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import TourGuides from "./pages/TourGuides"
+import GuideProfile from './pages/GuideProfile';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          <Routes>
-            <Route path="/EventTryoutPage" element={<EventTryoutPage />}></Route>
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<PlanExperience />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/pages/Home" element={<Home/>} />
+          <Route path="/pages/TourGuides" element={<TourGuides />} />
+          <Route path="/guide/:guideName" element={<GuideProfile />} />
+          <Route part="/pages/EventTryoutPage" element={<EventTryoutPage />} />
+        </Routes>
       </Router>
     </div>
   );
