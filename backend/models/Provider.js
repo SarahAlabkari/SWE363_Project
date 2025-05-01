4b228fa2d9bde7404b47099bd34381783472d98e
A0-2H <s202183890@kfupm.edu.sa>
2025-05-01 14:59:10 +0300

    Provider model: Ensure plain password is accepted and saved

--- backend/models/Provider.js
+++ backend/models/Provider.js
@@ -1,10 +1,13 @@
+// Path: backend/models/Provider.js
+
 const mongoose = require('mongoose');
 
 const providerSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  services: { type: String },
-  phone: { type: String }
+  companyName:   { type: String, required: true },
+  email:         { type: String, required: true, unique: true },
+  password:      { type: String, required: true }, // Stored as plain text
+  maaroofNumber: { type: String, required: true },
+  phoneNumber:   { type: String, required: true }
 });
 
 const Provider = mongoose.model('Provider', providerSchema);
